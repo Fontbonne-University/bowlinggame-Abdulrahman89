0290f006dfdb0dda13ae26798679bfb3ab057508
albishia <albishia@R416-15-780.Fontbonne.edu>
2019-03-26 15:26:17 -0500

    Bouling activity

--- BowlingGame/Bowling/src/BowlingGame/BowlingTest.java
+++ BowlingGame/Bowling/src/BowlingGame/BowlingTest.java
@@ -8,14 +8,69 @@ import org.junit.Test;
 
 public class BowlingTest {
 
+	private Game g;
+
 	@Before
-	public void setUp() throws Exception {
-
-	}
-
-	@Test
-	public void canCreateGame()
+	public void setUp()
 	{
-		Game g = new Game();
+		g = new Game();
 	}
+	@Test
+	public void test() {
+//		fail("Not yet implemented");
+	}
+	
+	private void rollMany(int j, int pins) {
+		for(int i=0; i< j; i++)
+			g.roll(pins);
+	}
+	
+	private void ollSpare() {
+		g.roll(5);
+		g.roll(5);
+	}
+	
+	@Test 
+	public void canRoll() {
+		Game g = new Game();
+		g.roll(0);
+	}
+	
+	@Test 
+	public void gutterGame( ) {
+		
+		rollMany(20, 0);
+		assertEquals(0, g.score());
+	}
+	
+	
+	@Test 
+	public void allOnes() {
+	
+		rollMany(20, 1);
+		assertEquals(20, g.score());
+		
+	}
+	
+	@Test
+	public void oneSpare() {
+		
+		ollSpare();	//spare
+		g.roll(3);
+		rollMany(17,0);
+		assertEquals(16,g.score());
+	}
+	
+	@Test
+	public void oneSike()
+	{
+		g.roll(10);
+		g.roll(3);
+		g.roll(4);
+		rollMany(16,0);
+		assertEquals(24, g.score());
+	}
+	
+
 }
+
--- BowlingGame/Bowling/src/BowlingGame/Game.java
+++ BowlingGame/Bowling/src/BowlingGame/Game.java
@@ -1,6 +1,37 @@
 package BowlingGame;
 
 public class Game {
-
+	
+	private int score = 0;
+	private int curentRoll = 0;
+	private int rolls[] = new int[21];
+	public void roll(int pins) {
+		// TODO Auto-generated method stub
+		rolls[curentRoll++] = pins;
+		
+		
+	}
+	
+	public int score() {
+		// TODO Auto-generated method stub
+		int fistInframe=0;
+		for(int frame =0; frame< 10; frame++)
+		{
+			if(rolls[fistInframe] + rolls[fistInframe+1] ==10) //spare
+			{
+				score += 10 + rolls[fistInframe+2];
+				fistInframe += 2;
+			}
+		
+			else {
+				//if(rolls[i] + rolls[i+1] == 10)
+				score += rolls[fistInframe] + rolls[fistInframe+1];
+				fistInframe += 2;
+			}
+		}
+		return score;
+		
+	}
 
 }
+	
